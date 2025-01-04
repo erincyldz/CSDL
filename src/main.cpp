d06ca16f6f824d1ca95bef174b44e9f4296f12e4
ecrinyildiz <ecryildiz@gmail.com>
2025-01-04 18:41:48 +0300

    [~] Manual Game Object: - Add manual game object for `Test` purposes

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,22 @@ int main(int argc, char* argv[])
     }
 
     game::Game game;
-    game.addRandomGameObject(5);
+    // game.addRandomGameObject(15);
+    std::string logger = "Circle";
+    auto circle1 = std::make_unique<game::object::CircleObject>(5, logger);
+    auto circle2 = std::make_unique<game::object::CircleObject>(5, logger);
+    game::object::Position pos1 = {game.getWindowWidth() / 2 - 100, game.getWindowHeight() / 2};
+    game::object::Position pos2 = {game.getWindowWidth() / 2, game.getWindowHeight() / 2 + 100};
+    circle1->setPosition(pos1);
+    circle2->setPosition(pos2);
+
+    game::object::Velocity vel1 = {50.0f, 0.0f};
+    game::object::Velocity vel2 = {0.0f, -50.0f};
+    circle1->setVelocity(vel1);
+    circle2->setVelocity(vel2);
+
+    game.addGameObject(std::move(circle1));
+    game.addGameObject(std::move(circle2));
+
     game.run();
 }
