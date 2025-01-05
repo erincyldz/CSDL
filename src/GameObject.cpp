1f6198307b5f8249c9880472c4469ddba7a60466
erincyldz <erincyildiz1@icloud.com>
2025-01-06 01:18:54 +0300

    [~?] Delta Time: Remove delta-time factor from collision resolving. It doesn't work like it suppose to
    Issue: #21

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -199,8 +199,8 @@ bool GameObject::is_colliding_with(const GameObject& other) const
 void GameObject::on_collision(GameObject& other, float delta_time)
 {
     // Calculate normal vector between objects
-    double normalX = (other.m_pos.x - this->m_pos.x) * delta_time;
-    double normalY = (other.m_pos.y - this->m_pos.y) * delta_time;
+    double normalX = (other.m_pos.x - this->m_pos.x);
+    double normalY = (other.m_pos.y - this->m_pos.y);
     double magnitude = std::sqrt(normalX * normalX + normalY * normalY);
     // Prevent division by zero or exact overlap
     if (magnitude == 0.0f)
