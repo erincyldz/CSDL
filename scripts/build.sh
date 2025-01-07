2e175432b5ff1fe90fe534f335ec0d6f959d1e0a
erincyldz <53654423+erincyldz@users.noreply.github.com>
2025-01-07 23:34:05 +0300

    Update build.sh
    Path specifiers included

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,14 @@
-#! /bin/bash
-cd ./build
-make -j12
+#!/bin/bash
+
+# Get the directory of the current script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$SCRIPT_DIR/.." # Adjust to point to ProjectRoot
+
+# Create and navigate to the build directory
+BUILD_DIR="$PROJECT_ROOT/build"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR" || exit
+
+# Run CMake and build
+cmake ..
+make
