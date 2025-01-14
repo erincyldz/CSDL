1531c66fe59e8df09e6cc1cfc32a9e3de9e8d14b
ecrinyildiz <ecryildiz@gmail.com>
2025-01-14 23:24:31 +0300

    [~] Test Case:
    - Adjust gravitational constant and modify circle object initialization parameters

--- src/main.cpp
+++ src/main.cpp
@@ -23,25 +23,29 @@ int main(int argc, char* argv[])
     std::string logger2 = "Circle2";
     std::string logger3 = "Circle3";
     std::string loggerr = "Rect";
-    auto circle1 = std::make_unique<game::object::CircleObject>(15, logger1);
+    auto circle1 = std::make_unique<game::object::CircleObject>(5, logger1);
     auto circle2 = std::make_unique<game::object::CircleObject>(15, logger2);
     auto circle3 = std::make_unique<game::object::CircleObject>(15, logger3);
     float width = game.getWindowWidth();
     float height = game.getWindowHeight();
-    game::object::Position pos1 = {width / 2 + 250, height / 2};
-    game::object::Position pos2 = {width / 2 - 250, height / 2};
+    game::object::Position pos1 = {width / 2 - 250, height / 2};
+    game::object::Position pos2 = {width / 2 + 250, height / 2};
     game::object::Position pos3 = {width / 2, height / 2 - 200};
     circle1->setPosition(pos1);
     circle2->setPosition(pos2);
     circle3->setPosition(pos3);
 
     circle1->setMass(1.0);
-    circle2->setMass(1.0);
-    circle3->setMass(1.0);
+    circle2->setMass(50000.0);
+    circle3->setMass(100.0);
 
-    game::object::Velocity vel1 = {0.0f, 0.0f};
-    game::object::Velocity vel2 = {0.0f, 0.0f};
-    game::object::Velocity vel3 = {0.0f, +500.0f};
+    circle1->setRestitution(1.0f);
+    circle2->setRestitution(1.0f);
+    circle3->setRestitution(1.0f);
+
+    game::object::Velocity vel1 = {60.0f, -100.0f};
+    game::object::Velocity vel2 = {-30.0f, 125.0f};
+    game::object::Velocity vel3 = {-20.0f, +100.0f};
     circle1->setVelocity(vel1);
     circle2->setVelocity(vel2);
     circle3->setVelocity(vel3);
